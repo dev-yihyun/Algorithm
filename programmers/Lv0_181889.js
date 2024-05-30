353901a7d53df170ee7b1400bdb6bcc37a42d3cc
dev-yihyun <dev_yihyun@naver.com>
2024-05-30 13:36:41 +0900

    [Programmers - 181889] n 번째 원소까지

---
+++ programmers/Lv0_181889.js
@@ -0,0 +1,12 @@
+// [Programmers - 181889] n 번째 원소까지
+
+function solution(num_list, n) {
+    var answer = num_list.slice(0,n);
+    return answer;
+}
+
+/*
+function solution(num_list, n) {
+    return num_list.slice(0, n);
+}
+*/

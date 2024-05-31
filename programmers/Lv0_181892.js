27acaa91217aacf5d81a0c88578ff19981e3d8ca
dev-yihyun <dev_yihyun@naver.com>
2024-05-31 13:34:13 +0900

    [Programmers - 181892] n 번째 원소부터

---
+++ programmers/Lv0_181892.js
@@ -0,0 +1,12 @@
+// [Programmers - 181892] n 번째 원소부터
+
+function solution(num_list, n) {
+    var answer = num_list.slice(n-1)
+    return answer;
+}
+
+/* 
+function solution(num_list, n) {
+  return num_list.slice(n - 1);
+}
+*/

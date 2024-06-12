14cab0fbfc53e9d903e6f3cee06127d82ab79862
dev-yihyun <dev_yihyun@naver.com>
2024-06-12 14:27:24 +0900

    [Programmers - 181840] 정수 찾기

---
+++ programmers/Lv0_181840.js
@@ -0,0 +1,8 @@
+// [Programmers - 181840] 정수 찾기
+
+function solution(num_list, n) {
+    var answer = num_list.includes(n)?1:0;
+    return answer;
+}
+
+/* const solution=(l,n)=>+(l.includes(n)) */

5fddbe8b3c56b3b2cbde9c8bde7f185cb8f74368
dev-yihyun <dev_yihyun@naver.com>
2025-09-12 18:47:35 +0900

    [Programmers - 181937] n의 배수

---
+++ programmers/Lv0_181937.js
@@ -0,0 +1,16 @@
+// [Programmers - 181937] n의 배수
+
+function solution(num, n) {
+    var answer = 0;
+    if(num%n==0){
+        return 1
+    }
+    
+    return answer;
+}
+
+/* 
+function solution(num, n) {
+    return num%n===0?1:0;
+}
+*/
